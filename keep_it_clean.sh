e7a8bd2a8a355d972a8a8bbec73ea7b4226880c1
kbstn <konradbe@web.de>
2023-02-20 23:36:14 +0100

    Update keep_it_clean.sh

--- keep_it_clean.sh
+++ keep_it_clean.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Set the directory to check and the number of days
-directory="/home/user/Downloads/"
+directory="/home/user/test/"
 days=7
 
 # Check if the directory exists
 if [ -d "$directory" ]; then
-    # Find all files in the directory older than the specified number of days and move them to the trash
-    deleted_files=$(find "$directory" * -mtime +7 -type f -exec trash {} \; -print)
+    # Find all files in the directory older than the specified number of days a>
+    deleted_files=$(find "$directory" * -mtime +7 -type f -exec trash {} \; -pr>
     find "$directory" * -mtime +7 -type f -exec rm {} \;
     # remove empty dirs afterwards
     find "$directory" -empty -type d -delete
@@ -15,8 +15,9 @@ if [ -d "$directory" ]; then
     # Check if any files were deleted
     if [ -n "$deleted_files" ]; then
         # Display a notification notifying the user that files were deleted
-        notify-send "Deleted files" "The following files were deleted:\n$deleted_files"
+        notify-send "Deleted files" "The following files were deleted because t>
     fi
 else
     echo "Directory does not exist"
 fi
+
